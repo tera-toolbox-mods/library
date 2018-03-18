2548ba74384c852d33fa01a2e6343785101be578
Kasea <1997xeela@gmail.com>
2018-03-18 05:53:59 +0100

    .

--- class/library.js
+++ class/library.js
@@ -13,6 +13,10 @@ class Library{
         return false;
     }
 
+    dist2D(loc1, loc2) {
+        return Math.sqrt(Math.pow(loc2.x - loc1.x, 2) + Math.pow(loc2.y - loc1.y, 2));
+    }
+
     dist3D(loc1, loc2) {
         return Math.sqrt(Math.pow(loc2.x - loc1.x, 2) + Math.pow(loc2.y - loc1.y, 2) + Math.pow(loc2.z - loc1.z, 2))
     }
